1439f58c8962fd46c6efe8a3a35f39b85c4f55e0
minglii <minglii@foxmail.com>
2019-04-20 14:08:25 +0800

    Add function of transmit file.

--- tcp_client.py
+++ tcp_client.py
@@ -1,22 +1,85 @@
 from socket import *
+import struct
+import json
+import os
+import sys
+import time
  
 HOST = "localhost"
 PORT = 6000
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
 wantStop = "Quit"
+f1Name = "file1.docx"
+f2Name = "file2.docx"
 
 sockCli = socket(AF_INET, SOCK_STREAM)
 sockCli.connect(ADDR)
  
 while True:
-        data = input(">")
-        if (data == wantStop):
-                break
-        sockCli.send(data.encode('utf-8'))
-        data = sockCli.recv(BUFSIZE)
-        if not data:
-                break
-        print (data)
+		#data = sockCli.recv(BUFSIZE)
+		#print(data)
+		data = input(">")
+		if (data == wantStop):
+			break
+		#sockCli.send(data.encode('utf-8'))
+		#data = sockCli.recv(BUFSIZE)
+		if (data == f1Name):
+			sockCli.send(data.encode('utf-8'))
+			time.sleep(0.1)
+			head_struct = sockCli.recv(4)
+			print ()
+			if head_struct:
+				print('Waiting for file1!')
+				head_len = struct.unpack('i', head_struct)[0]
+				data = sockCli.recv(head_len)
+				head_dir = json.loads(data.decode('utf-8'))
+				data = "Transmit mode!"
+				filesize_b = head_dir['filesize_bytes']
+				file_name = head_dir['file_name']
+				recv_len = 0
+				recv_mesg = b''
+				f = open(file_name, 'wb')
+				while recv_len < filesize_b:
+					if filesize_b - recv_len > BUFSIZE:
+						recv_mesg = sockCli.recv(BUFSIZE)
+						f.write(recv_mesg)
+						recv_len += len(recv_mesg)
+					else:
+						recv_mesg = sockCli.recv(filesize_b - recv_len)
+						recv_len += len(recv_mesg)
+						f.write(recv_mesg)
+						print("File1 transmitted!")
+		if (data == f2Name):
+			sockCli.send(data.encode('utf-8'))
+			time.sleep(0.1)
+			head_struct = sockCli.recv(4)
+			print ()
+			if head_struct:
+				print('Waiting for file2!')
+				head_len = struct.unpack('i', head_struct)[0]
+				data = sockCli.recv(head_len)
+				head_dir = json.loads(data.decode('utf-8'))
+				data = "Transmit mode!"
+				filesize_b = head_dir['filesize_bytes']
+				file_name = head_dir['file_name']
+				recv_len = 0
+				recv_mesg = b''
+				f = open(file_name, 'wb')
+				while recv_len < filesize_b:
+					if filesize_b - recv_len > BUFSIZE:
+						recv_mesg = sockCli.recv(BUFSIZE)
+						f.write(recv_mesg)
+						recv_len += len(recv_mesg)
+					else:
+						recv_mesg = sockCli.recv(filesize_b - recv_len)
+						recv_len += len(recv_mesg)
+						f.write(recv_mesg)
+						print("File2 transmitted!")
+		if (data != "Transmit mode!"):
+			sockCli.send(data.encode('utf-8'))
+			time.sleep(0.1)
+			data = sockCli.recv(BUFSIZE)
+			print (data)
  
 sockCli.close()
--- tcp_server.py
+++ tcp_server.py
@@ -1,5 +1,9 @@
 from socket import *
 from time import ctime
+import struct
+import json
+import os
+import sys
  
 HOST = ""
 PORT = 6000
@@ -16,14 +20,16 @@ unameWrong = "You enter a wrong user!"
 needPw = "Please enter your password!"
 pwWrong = "You enter a wrong password!"
 PW = "jsj623940"
-connSucc = "Connect sucessfully! You can begin transmit data!"
+connSucc = "Connect sucessfully! We have 2 files, enter file1.docx or file2.docx to get it."
 wantStop = "Quit"
 rece = "received:"
+f1Name = "file1.docx"
+f2Name = "file2.docx"
  
 while True:
 		sockCli,addr = sockSrv.accept()
 		print ('...connected from:',addr)	
-		sockCli.send(needUname.encode('utf-8'))
+		#sockCli.send(needUname.encode('utf-8'))
 		while True:
 				data = sockCli.recv(BUFSIZE)
 				if ((data.decode('utf-8') == user1 )or(data.decode('utf-8') == user2)):
@@ -36,10 +42,41 @@ while True:
 								data = sockCli.recv(BUFSIZE)
 								if (data.decode('utf-8') == wantStop):
 									break
+								elif (data.decode('utf-8') == f1Name):
+									filemsg = f1Name
+									filesize_bytes = os.path.getsize(filemsg)
+									file_name = 'new' + f1Name
+									dric = {
+										'file_name': file_name,
+										'filesize_bytes': filesize_bytes,
+									}
+									head_info = json.dumps(dric)
+									head_info_len = struct.pack('i', len(head_info))
+									sockCli.send(head_info_len)
+									sockCli.send(head_info.encode('utf-8'))
+									with open (filemsg,'rb') as f:
+										data = f.read()
+										sockCli.sendall(data)
+									print('Sended Successfully!')
+								elif (data.decode('utf-8') == f2Name):
+									filemsg = f2Name
+									filesize_bytes = os.path.getsize(filemsg)
+									file_name = 'new' + f2Name
+									dric = {
+										'file_name': file_name,
+										'filesize_bytes': filesize_bytes,
+									}
+									head_info = json.dumps(dric)
+									head_info_len = struct.pack('i', len(head_info))
+									sockCli.send(head_info_len)
+									sockCli.send(head_info.encode('utf-8'))
+									with open (filemsg,'rb') as f:
+										data = f.read()
+										sockCli.sendall(data)
+									print('Sended Successfully!')
 								else:
 									print(data)
 									sockCli.send(rece.encode('utf-8'))
-									sockCli.send(data)
 						elif (data.decode('utf-8') == wantStop):
 							break
 						else :
