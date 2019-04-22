2b206f1188c07057a7ae97ce1d7e9aaaa5bac316
minglii <minglii@foxmail.com>
2019-04-22 08:56:44 +0800

    TCP can type file name and IP addr.

--- tcp_server.py
+++ tcp_server.py
@@ -5,6 +5,7 @@ import json
 import os
 import sys
  
+#HOST=input("请输入Client IP地址：")
 HOST = ""
 PORT = 6000
 BUFSIZE = 1024
@@ -14,12 +15,12 @@ sockSrv = socket(AF_INET, SOCK_STREAM)
 sockSrv.bind(ADDR)
 sockSrv.listen(5)
 user1 = "2160500040"
-user2 = "2160500039"
+user2 = "2160500038"
 needUname = "Please enter your user name!"
 unameWrong = "You enter a wrong user!"
 needPw = "Please enter your password!"
 pwWrong = "You enter a wrong password!"
-PW = "jsj623940"
+PW = "jsj623840"
 connSucc = "Connect sucessfully! We have 2 files, enter file1.docx or file2.docx to get it."
 wantStop = "Quit"
 rece = "received:"
@@ -42,10 +43,10 @@ while True:
 								data = sockCli.recv(BUFSIZE)
 								if (data.decode('utf-8') == wantStop):
 									break
-								elif (data.decode('utf-8') == f1Name):
-									filemsg = f1Name
+								elif (data.decode('utf-8') == 'file1.docx' or data.decode('utf-8') == 'file2.docx'):
+									filemsg = data.decode('utf-8')
 									filesize_bytes = os.path.getsize(filemsg)
-									file_name = 'new' + f1Name
+									file_name = 'new' + data.decode('utf-8')
 									dric = {
 										'file_name': file_name,
 										'filesize_bytes': filesize_bytes,
@@ -57,23 +58,7 @@ while True:
 									with open (filemsg,'rb') as f:
 										data = f.read()
 										sockCli.sendall(data)
-									print('Sended Successfully!')
-								elif (data.decode('utf-8') == f2Name):
-									filemsg = f2Name
-									filesize_bytes = os.path.getsize(filemsg)
-									file_name = 'new' + f2Name
-									dric = {
-										'file_name': file_name,
-										'filesize_bytes': filesize_bytes,
-									}
-									head_info = json.dumps(dric)
-									head_info_len = struct.pack('i', len(head_info))
-									sockCli.send(head_info_len)
-									sockCli.send(head_info.encode('utf-8'))
-									with open (filemsg,'rb') as f:
-										data = f.read()
-										sockCli.sendall(data)
-									print('Sended Successfully!')
+									print('sended successfully!')
 								else:
 									print(data)
 									sockCli.send(rece.encode('utf-8'))
